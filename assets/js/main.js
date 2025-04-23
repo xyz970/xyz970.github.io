b71cc4ad4cbae22a31084bf749ff5b34af3f9eb8
mhmmdadisptro <muhammadxxz7@gmail.com>
2025-04-24 02:46:23 +0700

    RESURECT THE PROJECT
    Update tailwindcss version
    Add new error style
    Change about section style
    Initiate three js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,96 +1,112 @@
-var toogle = $('#toogle');
+// import "./animation.js";
+var toogle = $("#toogle");
 toogle.click(function (e) {
-    var hidden = $("#navbar-default").toggleClass("hidden");
-    if (!$("#navbar-default").is(":hidden")) {
-        $('#icon').removeClass('fa-bars').addClass('fa-xmark')
-    }else{
-        $('#icon').removeClass('fa-xmark').addClass('fa-bars')
+  var hidden = $("#navbar-default").toggleClass("hidden");
+  if (!$("#navbar-default").is(":hidden")) {
+    $("#icon").removeClass("fa-bars").addClass("fa-xmark");
+  } else {
+    $("#icon").removeClass("fa-xmark").addClass("fa-bars");
+  }
+  // console.log( $("#navbar-default").is(":hidden"));
+});
+
+$(document).ready(function () {
+  $("#x-animate").removeClass("translate-y-[150%]");
+  $("#x-animate").addClass("translate-y-0");
+
+  // setTimeout(() => {
+  //     $('#svg-x').addClass('animate-none');
+  // }, 4000);
+});
+
+$(window).resize(function () {
+  if ($(window).width() < 1200) {
+    //   alert("Less than 960");
+    if ($("#responsive-dialog").length < 1) {
+      $("#navbar").after(
+        `
+    <div class=" fixed z-[99] w-screen h-screen bg-black m-auto flex justify-center flex-col " id="responsive-dialog">
+        <div class="">
+            <h1 class="glitch text-center text-8xl font-extrabold font-[Pixelify_Sans] text-white" data-text="Ooppss, Didn't Find Anything?">Ooppss, Didn't Find Anything?</h1>
+          </div>
+          <div class="pt-3.5">
+            <h2 class="text-center text-3xl font-[Pixelify_Sans] text-white">Please use desktop version to continue</h2>
+          </div>
+    </div>`
+      );
     }
-    // console.log( $("#navbar-default").is(":hidden"));
-})
+  } else {
+    //   alert("More than 960");
+    $("#responsive-dialog").remove();
+  }
+});
 
-// document.body.onmousemove = function(e) {
-//     document.documentElement.style.setProperty (
-//       '--x', (
-//         e.clientX+window.scrollX
-//       )
-//       + 'px'
-//     );
-//     document.documentElement.style.setProperty (
-//       '--y', (
-//         e.clientY+window.scrollY
-//       ) 
-//       + 'px'
-//     );
-//   }
-cursor = $('#cursor')
-$(document).mousemove(function(event){
-    cursor.css(
-        {
-            'left': event.clientX+'px',
-            'top': event.clientY+'px'
-        }
-    ) 
-})
-$('a').mouseover(function(){
-    cursor.css({
-        'width':'70px',
-        'height':'70px',
-    })
-})
-$('a').mouseout(function(){
-    cursor.css({
-        'width':'120px',
-        'height':'120px',
-    })
-})
+cursor = $("#cursor");
+$(document).mousemove(function (event) {
+  cursor.css({
+    left: event.clientX + "px",
+    top: event.clientY + "px",
+  });
+});
+$("a").mouseover(function () {
+  cursor.css({
+    width: "70px",
+    height: "70px",
+  });
+});
+$("a").mouseout(function () {
+  cursor.css({
+    width: "120px",
+    height: "120px",
+  });
+});
 
-$('img').mouseover(function(){
-    cursor.css({
-        'width':'-10px',
-        'height':'-10px',
-    })
-})
-$('img').mouseout(function(){
-    cursor.css({
-        'width':'120px',
-        'height':'120px',
-    })
-})
+$("img").mouseover(function () {
+  cursor.css({
+    width: "-10px",
+    height: "-10px",
+  });
+});
+$("img").mouseout(function () {
+  cursor.css({
+    width: "120px",
+    height: "120px",
+  });
+});
 
-$('button').mouseover(function(){
-    cursor.css({
-        'width':'-10px',
-        'height':'-10px',
-    })
-})
-$('button').mouseout(function(){
-    cursor.css({
-        'width':'120px',
-        'height':'120px',
-    })
-})
+$("button").mouseover(function () {
+  cursor.css({
+    width: "-10px",
+    height: "-10px",
+  });
+});
+$("button").mouseout(function () {
+  cursor.css({
+    width: "120px",
+    height: "120px",
+  });
+});
 
-$('card').mouseover(function(){
-    cursor.css({
-        'width':'-10px',
-        'height':'-10px',
-    })
-})
-$('card').mouseout(function(){
-    cursor.css({
-        'width':'120px',
-        'height':'120px',
-    })
-})
+$("card").mouseover(function () {
+  cursor.css({
+    width: "-10px",
+    height: "-10px",
+  });
+});
+$("card").mouseout(function () {
+  cursor.css({
+    width: "120px",
+    height: "120px",
+  });
+});
 
 const sr = ScrollReveal({
-    origin: 'top',
-    distance: '30px',
-    duration: 2000,
-    reset: true
+  origin: "top",
+  distance: "30px",
+  duration: 2000,
+  reset: true,
 });
 
 sr.reveal(`#home, #about`, {
-    interval: 400
-})
+  interval: 400,
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {
+      animation: {
+        'spin-slow': 'spin 3s linear infinite',
+       },
       scale: ["hover", "active", "group-hover"],
     },
   },
